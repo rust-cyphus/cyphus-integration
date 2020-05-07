95673fbddda192fef1ce0355149962c476ecb8d4
Logan A Morrison <loanmorr@ucsc.com>
2020-05-07 00:23:07 -0700

    Adding new modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+pub(crate) mod error;
 pub(crate) mod extrap;
+pub(crate) mod qag;
 pub(crate) mod qk;
 pub(crate) mod sort;
+pub(crate) mod test_utils;
+pub(crate) mod utils;
 pub(crate) mod workspace;

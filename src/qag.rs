36069632b5aeabcde09493c451339b61b9504cf3
Logan A Morrison <loanmorr@ucsc.com>
2020-05-07 15:48:47 -0700

    Bug fix in how iter was updated & added tests

--- src/qag.rs
+++ src/qag.rs
@@ -11,7 +11,7 @@ pub fn qag<F>(
     epsabs: f64,
     epsrel: f64,
     limit: usize,
-    key: i8,
+    key: u8,
 ) -> std::result::Result<(f64, f64), IntegrationRetcode>
 where
     F: Fn(f64) -> f64,
@@ -30,7 +30,7 @@ where
     let mut abserr = 0.0;
 
     // Check the tolerances
-    if epsabs <= 0.0 && epsrel < 50.0 * f64::EPSILON {
+    if epsabs <= 0.0 && (epsrel < 50.0 * f64::EPSILON || epsrel < 0.5e-28) {
         return handle_error(result, abserr, IntegrationRetcode::BadTol);
     }
 
@@ -40,6 +40,7 @@ where
     result = fixed_order_gauss_kronrod(&f, a, b, key, &mut abserr, &mut resabs, &mut resasc);
     workspace.rlist[0] = result;
     workspace.elist[0] = abserr;
+    workspace.size = 1;
 
     // Test on accuracy
     let mut tolerance = epsabs.max(epsrel * result.abs());
@@ -49,9 +50,7 @@ where
 
     if abserr <= round_off && abserr > tolerance {
         return handle_error(result, abserr, IntegrationRetcode::RoundOffFirstIter);
-    } else if (abserr <= tolerance && (abserr - resasc).abs() > f64::EPSILON)
-        || abserr.abs() < f64::EPSILON
-    {
+    } else if (abserr <= tolerance && abserr != resasc) || abserr.abs() < f64::EPSILON {
         return Ok((result, abserr));
     } else if limit == 1 {
         return handle_error(result, abserr, IntegrationRetcode::OneIterNotEnough);
@@ -87,9 +86,9 @@ where
         let error12 = error1 + error2;
 
         errsum += error12 - e_i;
-        area = area + area12 - r_i;
+        area += area12 - r_i;
 
-        if (resabs1 - error1).abs() > f64::EPSILON && (resasc2 - error2).abs() > f64::EPSILON {
+        if resasc1 != error1 && resasc2 != error2 {
             let delta = r_i - area12;
 
             if delta.abs() <= 1e-5 * area12.abs() && error12 >= 0.99 * e_i {
@@ -115,11 +114,10 @@ where
 
         workspace.update((a1, b1, area1, error1), (a2, b2, area2, error2));
 
+        iter += 1;
         if iter >= limit || error_type != IntegrationRetcode::Success || errsum <= tolerance {
             break;
         }
-
-        iter += 1;
     }
 
     result = workspace.sum_results();
@@ -137,7 +135,98 @@ where
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::test_utils::test_rel;
+
+    fn f1(x: f64, alpha: f64) -> f64 {
+        x.powf(alpha) * x.recip().ln()
+    }
+
+    fn f3(x: f64, alpha: f64) -> f64 {
+        (2f64.powf(alpha) * x.sin()).cos()
+    }
+
+    fn f16(x: f64, alpha: f64) -> f64 {
+        if x == 0.0 && alpha == 1.0 {
+            1.0
+        } else if x == 0.0 && alpha > 1.0 {
+            0.0
+        } else {
+            x.powf(alpha - 1.0) * (1.0 + 10.0 * x).recip().powi(2)
+        }
+    }
 
     #[test]
-    fn test_gauss() {}
+    fn test_smooth_15() {
+        let alpha = 2.6;
+        let f = |x| f1(x, alpha);
+
+        let exp_result = 7.716049382715854665E-02;
+        let exp_abserr = 6.679384885865053037E-12;
+
+        let result = qag(f, 0.0, 1.0, 0.0, 1e-10, 1000, 1);
+
+        match result {
+            Ok(result) => {
+                test_rel(result.0, exp_result, 1e-15);
+                test_rel(result.1, exp_abserr, 1e-6);
+            }
+            Err(err) => panic!("{:?}", err),
+        }
+
+        let result = qag(f, 1.0, 0.0, 0.0, 1e-10, 1000, 1);
+
+        match result {
+            Ok(result) => {
+                test_rel(result.0, -exp_result, 1e-15);
+                test_rel(result.1, exp_abserr, 1e-6);
+            }
+            Err(err) => panic!("{:?}", err),
+        }
+    }
+
+    #[test]
+    fn test_roundoff_panic() {
+        let alpha = 1.3;
+        let f = |x| f3(x, alpha);
+
+        let result = qag(f, 0.3, 2.71, 1e-14, 0.0, 1000, 3);
+
+        match result {
+            Ok(_) => {
+                assert!(false);
+            }
+            Err(err) => assert!(
+                err == IntegrationRetcode::RoundOff || err == IntegrationRetcode::RoundOffFirstIter
+            ),
+        }
+    }
+
+    #[test]
+    fn test_singularity_detection() {
+        let alpha = 2.0;
+        let f = |x| f16(x, alpha);
+
+        let result = qag(f, -1.0, 1.0, 1e-14, 0.0, 1000, 5);
+
+        match result {
+            Ok(_) => {
+                assert!(false);
+            }
+            Err(err) => assert!(err == IntegrationRetcode::BadIntegrand),
+        }
+    }
+    #[test]
+    fn test_iter_limit() {
+        let alpha = 1.0;
+        let f = |x| f16(x, alpha);
+
+        let result = qag(f, -1.0, 1.0, 1e-14, 0.0, 1000, 6);
+
+        match result {
+            Ok(_) => {
+                assert!(false);
+            }
+            Err(err) => assert!(err == IntegrationRetcode::TooManyIters),
+        }
+    }
 }

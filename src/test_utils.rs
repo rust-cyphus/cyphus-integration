01ead3ca606606092f54cfd8ceee55e0b8c13be0
Logan A Morrison <loanmorr@ucsc.com>
2020-05-07 23:34:25 -0700

    Moved testing function to test_utils.rs

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -31,3 +31,38 @@ pub(crate) fn test_rel(result: f64, expected: f64, relative_error: f64) {
     );
     assert!(status != 1, "[Test uses subnormal value]".to_string());
 }
+
+/// Function 1 for integration testing
+pub(crate) fn f1(x: f64, alpha: f64) -> f64 {
+    x.powf(alpha) * x.recip().ln()
+}
+
+/// Function 3 for integration testing
+pub(crate) fn f3(x: f64, alpha: f64) -> f64 {
+    (2f64.powf(alpha) * x.sin()).cos()
+}
+
+/// Function 16 for integration testing
+pub(crate) fn f11(x: f64, alpha: f64) -> f64 {
+    x.recip().ln().powf(alpha - 1.0)
+}
+
+/// Function 15 for integration testing
+pub(crate) fn f15(x: f64, alpha: f64) -> f64 {
+    x * x * (-2f64.powf(-alpha) * x).exp()
+}
+
+/// Function 16 for integration testing
+pub(crate) fn f16(x: f64, alpha: f64) -> f64 {
+    if x == 0.0 && alpha == 1.0 {
+        1.0
+    } else if x == 0.0 && alpha > 1.0 {
+        0.0
+    } else {
+        x.powf(alpha - 1.0) * (1.0 + 10.0 * x).recip().powi(2)
+    }
+}
+
+pub(crate) fn f455(x: f64) -> f64 {
+    x.ln() / (1.0 + 100.0 * x * x)
+}

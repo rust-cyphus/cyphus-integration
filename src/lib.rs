87cde123d480ae86bb4862ad47254757b40e9c43
Logan A Morrison <loanmorr@ucsc.com>
2020-05-15 23:03:52 -0700

    rustfmt reorder

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 pub(crate) mod extrap;
 pub(crate) mod qag;
 pub(crate) mod qagi;
+pub(crate) mod qagp;
 pub(crate) mod qags;
 pub(crate) mod qk;
+pub mod result;
 pub(crate) mod sort;
 pub(crate) mod test_utils;
 pub(crate) mod utils;
 pub(crate) mod workspace;
-pub(crate) mod qagp;
-pub mod result;
